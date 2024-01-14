9f62cb2bc7e3e7a13d8a65fde10928841cf948ed
Anja Klaßen <anja.klassen@posteo.de>
2024-01-14 20:18:51 +0100

    add solution

--- src/App.js
+++ src/App.js
@@ -2,5 +2,35 @@ import React from "react";
 import "./styles.css";
 
 export default function App() {
-  return <h1>Replace me with your component!</h1>;
+  function handleClick() {
+    console.log("You clicked a button");
+  }
+  return (
+    <>
+      <Button
+        color="yellow"
+        disabled={false}
+        text="Click not!"
+        onClick={handleClick}
+      />
+      <Button
+        color="limegreen"
+        disabled={false}
+        text="Click!"
+        onClick={handleClick}
+      />
+    </>
+  );
+}
+
+function Button({ color, disabled, text, onClick }) {
+  return (
+    <button
+      disabled={disabled}
+      style={{ backgroundColor: color }}
+      onClick={onClick}
+    >
+      {text}
+    </button>
+  );
 }
